e02597a62ed5f00f662710f633f18526535d650f
Suraj Singh <iamlostknight7@gmail.com>
2024-02-11 12:26:20 +0100

    Add script to deploy contract to public test network

--- inbox/deploy.js
+++ inbox/deploy.js
@@ -1,5 +1,27 @@
+require('dotenv').config();
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const { Web3 } = require('web3');
-//updated web3 and hdwallet-provider imports added for convenience
+const { interface, bytecode } = require('./compile');
 
-// deploy code will go here
+const provider = new HDWalletProvider(
+    process.env.ACCOUNT_MNEMONIC,
+    process.env.TEST_NETWORK
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+    const accounts = await web3.eth.getAccounts();
+    console.log('Attempting to deploy from account', accounts[0]);
+
+    const inbox = await new web3.eth.Contract(JSON.parse(interface))
+        .deploy({ data: bytecode, arguments: [ 'Hi there public Test Network!' ] })
+        .send({ from: accounts[0], gas: '1000000' });
+    
+    console.log('Contract deployed to ', inbox.options.address);
+
+    // Stops the process after running above
+    provider.engine.stop();
+}
+
+deploy();
