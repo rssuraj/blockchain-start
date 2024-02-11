5abe282edae64988e4694edb930108efd86145fa
Suraj Singh <iamlostknight7@gmail.com>
2024-02-11 10:49:58 +0100

    Moved example test to sample.test file

---
+++ inbox/test/sample.test.js
@@ -0,0 +1,28 @@
+const assert = require('assert');
+
+// Example unit tests
+class Car {
+    park() {
+        return 'stopped';
+    }
+
+    drive() {
+        return 'vroom';
+    }
+}
+
+let car;
+
+beforeEach(() => {
+    car = new Car();
+});
+
+describe('Car', () => {
+    it('can park', () => {
+        assert.equal(car.park(), 'stopped');
+    });
+
+    it('can drive', () => {
+        assert.equal(car.drive(), 'vroom');
+    });
+});
