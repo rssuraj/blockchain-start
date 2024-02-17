18bc4b2c14f82435647a26ac5258b9663cebd713
Suraj Singh <iamlostknight7@gmail.com>
2024-02-17 16:56:43 +0100

    Modify Campaign.sol for solc version 0.8.9

--- kickstart/ethereum/contracts/Campaign.sol
+++ kickstart/ethereum/contracts/Campaign.sol
@@ -1,14 +1,16 @@
-pragma solidity ^0.4.17;
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.9;
 
 contract CampaignFactory {
     address[] public deployedCampaigns;
 
     function createCampaign(uint minimum) public {
-        address newCampaign = new Campaign(minimum, msg.sender);
-        deployedCampaigns.push(newCampaign);
+        Campaign newCampaign = new Campaign(minimum, msg.sender);
+        deployedCampaigns.push(address(newCampaign));
     }
 
-    function getDeployedCampaigns() public view returns (address[]) {
+    function getDeployedCampaigns() public view returns (address[] memory) {
         return deployedCampaigns;
     }
 }
@@ -17,62 +19,64 @@ contract Campaign {
     struct Request {
         string description;
         uint value;
-        address recipient;
+        address payable recipient;
         bool complete;
         uint approvalCount;
         mapping(address => bool) approvals;
     }
 
-    Request[] public requests;
     address public manager;
     uint public minimumContribution;
+    uint approversCount;
     mapping(address => bool) public approvers;
-    uint public approversCount;
+    uint requestCount;
+    mapping(uint => Request) public requests;
 
-    modifier restricted() {
+    modifier restrictedMinimum() {
+        require(msg.value > minimumContribution);
+        _;
+    }
+
+    modifier restrictedManager() {
         require(msg.sender == manager);
         _;
     }
 
-    function Campaign(uint minimum, address creator) public {
+    constructor(uint minimum, address creator) {
         manager = creator;
         minimumContribution = minimum;
     }
 
-    function contribute() public payable {
-        require(msg.value > minimumContribution);
-
+    function contribute() public payable restrictedMinimum {
         approvers[msg.sender] = true;
         approversCount++;
     }
 
-    function createRequest(string description, uint value, address recipient) public restricted {
-        Request memory newRequest = Request({
-           description: description,
-           value: value,
-           recipient: recipient,
-           complete: false,
-           approvalCount: 0
-        });
-
-        requests.push(newRequest);
+    function createRequest(string memory description, uint value, address recipient) 
+        public restrictedManager {
+            Request storage newRequest = requests[requestCount++];
+            newRequest.description = description;
+            newRequest.value = value;
+            newRequest.recipient = payable(recipient);
+            newRequest.complete = false;
+            newRequest.approvalCount = 0;
     }
 
     function approveRequest(uint index) public {
         Request storage request = requests[index];
 
-        require(approvers[msg.sender]);
-        require(!request.approvals[msg.sender]);
+        require(approvers[msg.sender], "Not an approver");
+        require(!request.approvals[msg.sender], "Already approved");
 
         request.approvals[msg.sender] = true;
         request.approvalCount++;
     }
 
-    function finalizeRequest(uint index) public restricted {
+    function finalizeRequest(uint index) public restrictedManager {
         Request storage request = requests[index];
 
-        require(request.approvalCount > (approversCount / 2));
-        require(!request.complete);
+        require(request.approvalCount > (approversCount / 2), "Approves must be more than 50%");
+        require(!request.complete, "Request already complete");
 
         request.recipient.transfer(request.value);
         request.complete = true;
