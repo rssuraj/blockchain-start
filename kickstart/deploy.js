3f4f3f8616ead75560de2eb3c7c21dfd37e8bbe9
Suraj Singh <iamlostknight7@gmail.com>
2024-02-18 12:09:14 +0100

    Moved deploy.js folder to root for env support

---
+++ kickstart/deploy.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const { Web3 } = require('web3');
+const { abi, evm } = require('./ethereum/build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  process.env.ACCOUNT_MNEMONIC,
+  process.env.TEST_NETWORK
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(abi)
+    .deploy({ data: evm.bytecode.object })
+    .send({ gas: '5000000', from: accounts[0] });
+
+  console.log(JSON.stringify(abi));
+  console.log("Contract deployed to", result.options.address);
+
+  provider.engine.stop();
+};
+
+deploy();
+// Contract address: 0x488d6612c09162F0e1d050eD8545609439db18e9
